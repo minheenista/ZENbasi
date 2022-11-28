90b18672f56d1053512abe3fb94958cb216e378c
minheenista <anamichelle1308@gmail.com>
2022-11-27 20:23:34 -0600

    first commit

---
+++ TODO/recuperarContra.js
@@ -0,0 +1,27 @@
+function recuperacionAlumno(){
+    /* const popup = document.getElementById('recuperarAlumno');
+                popup.classList.toggle('show') */
+    alert("Revise su correo");
+    swal("Correo enviado!", "Revisa tu correo de confirmacion alumno", "success");
+}
+
+function recuperacionDocente(){
+    /* const popup = document.getElementById('recuperarAlumno');
+                popup.classList.toggle('show') */
+    alert("Revise su correo");
+    swal("Correo enviado!", "Revisa tu correo de confirmacion docente", "success");
+}
+
+function recuperacionTutor(){
+    /* const popup = document.getElementById('recuperarAlumno');
+                popup.classList.toggle('show') */
+    alert("Revise su correo");
+    swal("Correo enviado!", "Revisa tu correo de confirmacion tutor", "success");
+}
+
+function recuperacionAdmin(){
+    /* const popup = document.getElementById('recuperarAlumno');
+                popup.classList.toggle('show') */
+    alert("Revise su correo");
+    swal("Correo enviado!", "Revisa tu correo de confirmacion administrador", "success");
+}
